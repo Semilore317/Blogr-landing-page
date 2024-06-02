e03399e565c15deaa668172ea1f48fea8365d995
Semilore317 <semi.banks.317@gmail.com>
2024-06-02 23:20:15 +0100

    hey

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const connectTab = document.querySelector(".connect");
 
+//mobile nav animation
+const burger = document.querySelector(".burger-div");
+const burgerDiv1 = document.querySelector(".burger-div-1");
+const burgerDiv2 = document.querySelector(".burger-div-2");
+const burgerDiv3 = document.querySelector(".burger-div-3");
+
+burger.addEventListener("click", () => {
+  burgerDiv1.style.transform = "rotate(45deg)";
+});
