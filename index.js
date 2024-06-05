b33d025b7b4093e23244ed6d1b1ba52bf3fc6b80
Semilore317 <semi.banks.317@gmail.com>
2024-06-05 10:16:27 +0100

    5th june

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const connectTab = document.querySelector(".connect");
+/*const connectTab = document.querySelector(".connect");
 
 //mobile nav animation
 const burger = document.querySelector(".burger-div");
@@ -6,8 +6,5 @@ const burgerDiv1 = document.querySelector(".burger-div-1");
 const burgerDiv2 = document.querySelector(".burger-div-2");
 const burgerDiv3 = document.querySelector(".burger-div-3");
 
-burger.addEventListener("click", () => {
-  burgerDiv1.style.transform = "rotate(45deg)";
-  burgerDiv2.style.display = "none";
-  burgerDiv3.style.transform = "rotate(45deg)";
-});
+burger.addEventListener('click', burgerFunction())
+*/
