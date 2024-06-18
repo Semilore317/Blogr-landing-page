73651134a2e18e208172b1bdf9dc4a1b7b86609f
Semilore317 <semi.banks.317@gmail.com>
2024-06-18 09:53:25 +0100

    hey

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/*const connectTab = document.querySelector(".connect");
+const connectTab = document.querySelector(".connect-after-div");
+const connectTabP = document.querySelectorAll(".connect-after-div P");
 
 //mobile nav animation
 const burger = document.querySelector(".burger-div");
@@ -6,5 +7,16 @@ const burgerDiv1 = document.querySelector(".burger-div-1");
 const burgerDiv2 = document.querySelector(".burger-div-2");
 const burgerDiv3 = document.querySelector(".burger-div-3");
 
-burger.addEventListener('click', burgerFunction())
-*/
+//event listener!
+//burger.addEventListener("dbclick", burgerFunction());
+//for some reason it's clicking itself, so i'm using onClick()
+
+//CTA - Call to Action dipshit
+function burgerFunction(){
+    //use toggle
+    console.log('yeahh');
+    connectTab.style.opacity = '1.0';
+    connectTabP.style.opacity = '1.0';
+    burger.style.backgroundColor = "red";
+    
+}
